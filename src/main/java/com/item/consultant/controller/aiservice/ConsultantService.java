4744e5bf095f430aa30f5f7d6df18eef4f98a01c
ztq <2287404995@qq.com>
2025-10-23 16:08:25 +0800

    feat(consultant-service): 引入系统消息配置支持 - 添加@SystemMessage(fromResource = "system.txt") 注解以支持从资源文件加载系统消息 - @UserMessage("你是东哥的小月月，人美心善又多金！{{msg}}")  需要使用@V("msg") 或者 @UserMessage("你是东哥的小月月，人美心善又多金！{{it}}") - 创建 system.txt 文件定义 AI 咨询师的行为和回复规则 - 配置 AI 咨询师专注于高考志愿填报相关问题 - 实现志愿填报功能指引及一对一预约服务流程 - 禁止回答与高考志愿填报无关的问题 - 规范预约信息收集方式，避免模拟用户数据

--- src/main/java/com/item/consultant/controller/aiservice/ConsultantService.java
+++ src/main/java/com/item/consultant/controller/aiservice/ConsultantService.java
@@ -1,6 +1,7 @@
 package com.item.consultant.controller.aiservice;
 
 
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 import reactor.core.publisher.Flux;
@@ -12,8 +13,17 @@ import reactor.core.publisher.Flux;
 )
 public interface ConsultantService {
     // 用于阻塞聊天的方法
+    @SystemMessage(fromResource = "system.txt") // 从 resources 目录加载系统消息文件
     String chat(String message);
 
     // 用于流式聊天的方法
+    // @SystemMessage("你是东哥的小月月，人美心善又多金！") // 直接使用字符串作为系统消息
+//    @UserMessage("你是东哥的小月月，人美心善又多金！{{it}}")
+//    Flux<String> chatStream(String message);
+
+//    @UserMessage("你是东哥的小月月，人美心善又多金！{{msg}}")
+//    Flux<String> chatStream(@V("msg") String message);
+    
+    @SystemMessage(fromResource = "system.txt") // 从 resources 目录加载系统消息文件
     Flux<String> chatStream(String message);
 }
