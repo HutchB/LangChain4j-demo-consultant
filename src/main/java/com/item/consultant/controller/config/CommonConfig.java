4b52fabb67c6d9a67f04032e8e345b87cda4217b
ztq <2287404995@qq.com>
2025-10-24 08:57:56 +0800

    feat(ai): 集成向量数据库检索功能
    - 添加 langchain4j-easy-rag依赖以支持 RAG 功能
    - 配置 application.yml 日志注释及调整日志级别
    - 在 CommonConfig 中实现构建向量数据库操作对象与构建向量数据库检索对象
    - 通过 ConsultantService 启用 contentRetriever 支持
    - 新增西北大学录取数据md 文本文档用于知识库检索

--- src/main/java/com/item/consultant/controller/config/CommonConfig.java
+++ src/main/java/com/item/consultant/controller/config/CommonConfig.java
@@ -1,13 +1,22 @@
 package com.item.consultant.controller.config;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class CommonConfig {
@@ -49,6 +58,32 @@ public class CommonConfig {
             }
         };
         return chatMemoryProvider;
+    }
+
+    // 构建向量数据库操作对象
+    @Bean
+    public EmbeddingStore store() {
+        // 1.加载文档进内存 类路径加载 文件磁盘加载 url加载
+        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
+        //List<Document> documents = FileSystemDocumentLoader.loadDocuments("D:\\codeSpace\\source\\CodeSource\\study\\consultant\\consultant\\src\\main\\resources\\content");
+        // 2.构建向量数据库操作对象
+        InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
+        // 3.构建一个embeddingStoreIngestor对象,完成文本数据切割，向量化，存储
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .embeddingStore(store)
+                .build();
+        ingestor.ingest(documents);
+        return store;
+    }
+
+    // 构建向量数据库检索对象
+    @Bean
+    public ContentRetriever contentRetriever(EmbeddingStore store) {
+        return EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(store)//设置向量数据库操作对象
+                .minScore(0.5) //设置最小分数
+                .maxResults(3) //设置最大片段数量
+                .build();
     }
 
 }
