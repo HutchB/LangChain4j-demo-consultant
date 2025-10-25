3aa0ced630db3971e8d2de6f46461aa1c42c4e49
ztq <2287404995@qq.com>
2025-10-25 21:20:57 +0800

    feat(config): 配置Redis向量存储并更新相关依赖 - 在application.yml中添加community.redis配置项 - 更新spring.redis.host地址为192.168.11.10 - 引入RedisEmbeddingStore依赖并移除InMemoryEmbeddingStore - 修改EmbeddingStore配置以使用Redis作为向量数据库 - 添加langchain4j-community-redis-spring-boot-starter依赖- 调整contentRetriever和store方法以适配Redis向量存储 - 设置max-segments-per-batch为10以优化批处理性能

--- src/main/java/com/item/consultant/controller/config/CommonConfig.java
+++ src/main/java/com/item/consultant/controller/config/CommonConfig.java
@@ -1,5 +1,6 @@
 package com.item.consultant.controller.config;
 
+import dev.langchain4j.community.store.embedding.redis.RedisEmbeddingStore;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
@@ -14,7 +15,6 @@ import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
-import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
@@ -30,6 +30,8 @@ public class CommonConfig {
     private ChatMemoryStore redisChatMemoryStore;
     @Resource
     private EmbeddingModel embeddingModel;
+    @Resource
+    private RedisEmbeddingStore redisEmbeddingStore;
 
 //    @Bean
 //    public ConsultantService consultantService(){
@@ -63,34 +65,34 @@ public class CommonConfig {
         return chatMemoryProvider;
     }
 
-    // 构建向量数据库操作对象
-    @Bean
+    // 构建向量数据库操作对象 只需要构建一次即可
+//    @Bean
     public EmbeddingStore store() {
         // 1.加载文档进内存 类路径加载 文件磁盘加载 url加载
         //List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
         //List<Document> documents = FileSystemDocumentLoader.loadDocuments("D:\\codeSpace\\source\\CodeSource\\study\\consultant\\consultant\\src\\main\\resources\\content");
         List<Document> documents = ClassPathDocumentLoader.loadDocuments("content", new ApachePdfBoxDocumentParser());
-        // 2.构建向量数据库操作对象
-        InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
+        // 2.构建向量数据库操作对象 操作的是内存版本的向量数据库
+        //InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
 
         // 构建文档分割器对象
         DocumentSplitter ds = DocumentSplitters.recursive(500, 100); // 每个片段字符数量 两片段重叠字数
 
         // 3.构建一个embeddingStoreIngestor对象,完成文本数据切割，向量化，存储
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-                .embeddingStore(store) // 设置向量数据库操作对象
+                .embeddingStore(redisEmbeddingStore) // 设置向量数据库操作对象
                 .documentSplitter(ds) // 设置文档分割器对象
                 .embeddingModel(embeddingModel) // 设置向量化模型对象
                 .build();
         ingestor.ingest(documents);
-        return store;
+        return redisEmbeddingStore;
     }
 
     // 构建向量数据库检索对象
     @Bean
     public ContentRetriever contentRetriever(EmbeddingStore store) {
         return EmbeddingStoreContentRetriever.builder()
-                .embeddingStore(store)//设置向量数据库操作对象
+                .embeddingStore(redisEmbeddingStore)//设置向量数据库操作对象
                 .minScore(0.5) //设置最小分数
                 .maxResults(3) //设置最大片段数量
                 .embeddingModel(embeddingModel) // 设置向量化模型对象
